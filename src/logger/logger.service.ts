158b5899de78fe0dfb6a7575b3c5e92afa60b22f
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2022-12-06 18:47:30 +0400

    Added logger

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,21 @@
+import { Logger } from 'tslog';
+
+export class LoggerService {
+  public logger: Logger<unknown>;
+
+  constructor() {
+    this.logger = new Logger()
+  }
+
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
+}
